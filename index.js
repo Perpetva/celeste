0aa7bfc87e2ae68b1fbe9f4586698e1d707fb88c
Richard Martins <130320916+Perpetva@users.noreply.github.com>
2026-08-04 00:21:27 -0300

    Change cron schedule time from '1 0' to '30 0'

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ async function start() {
     await tableReservation()
 }
 
-cron.schedule('1 0 * * *', async () => {
+cron.schedule('30 0 * * *', async () => {
     try {
         console.log('Execução agendada iniciada em ' + new Date().toLocaleString())
         await start()
